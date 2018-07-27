af6ff182e47c21fb73b279983ddcc85e5cd4b655
valda-z <valdemar.zavadsky@microsoft.com>
2018-07-27 21:55:11 +0200

    search from dashboard

--- NonProfitCRM/Controllers/HomeController.cs
+++ NonProfitCRM/Controllers/HomeController.cs
@@ -16,12 +16,63 @@ namespace NonProfitCRM.Controllers
         {
             return RedirectToAction("Dashboard");
         }
-        public ActionResult Dashboard()
+        public ActionResult Dashboard(string search)
         {
+            if (search == null)
+            {
+                search = "";
+            }
+            bool showDeleted = (Request.Cookies["nonprofitorgIsDelOn"]?.Value == "true");
             var cx = new Entities();
             var model = new HomeModel();
+            model.Search = search.Trim();
             model.TaskList = cx.ViewTaskList.
                 Where(e => e.StatusId < 1000).OrderBy(e=>e.DueDate);
+
+            if (model.Search.Length > 0)
+            {
+                model.EventList = new Entities().ViewEventList.
+                    Where(e => (search == "" ||
+                        (e.CompanyName.StartsWith(search) ||
+                            e.Name.StartsWith(search) ||
+                            e.ContactCompanyName.StartsWith(search) ||
+                            e.NonProfitOrgName.StartsWith(search) ||
+                            e.ContactNonProfitOrgName.StartsWith(search)
+                        ))
+                        && (showDeleted || !e.Deleted)).
+                    OrderByDescending(e => e.DateOfEvent).Take(Properties.Settings.Default.MAXRECORDS);
+
+                model.CompanyList = new Entities().ViewCompanyList.
+                    Where(e => (search == "" ||
+                        (e.IdentificationNumber.StartsWith(search) ||
+                            e.Name.StartsWith(search) ||
+                            e.Address.StartsWith(search) ||
+                            e.City.StartsWith(search) ||
+                            e.Contact1Name.StartsWith(search) ||
+                            e.Contact2Name.StartsWith(search) ||
+                            e.CountryName.StartsWith(search) ||
+                            e.RegionName.StartsWith(search) ||
+                            e.Www.StartsWith(search)
+                        )) && (showDeleted || !e.Deleted)).
+                    OrderBy(e => e.Name).Take(Properties.Settings.Default.MAXRECORDS);
+
+                model.NonProfitOrgList = new Entities().ViewNonProfitOrgList.
+                    Where(e =>
+                        (search == "" ||
+                            (e.IdentificationNumber.StartsWith(search) ||
+                                e.Name.StartsWith(search) ||
+                                e.Address.StartsWith(search) ||
+                                e.City.StartsWith(search) ||
+                                e.Contact1Name.StartsWith(search) ||
+                                e.Contact2Name.StartsWith(search) ||
+                                e.CountryName.StartsWith(search) ||
+                                e.RegionName.StartsWith(search) ||
+                                e.Www.StartsWith(search)
+                            )
+                        ) && (showDeleted || !e.Deleted)).
+                    OrderBy(e => e.Name).Take(Properties.Settings.Default.MAXRECORDS);
+            }
+
             return View(model);
         }
     }
--- NonProfitCRM/Models/HomeModel.cs
+++ NonProfitCRM/Models/HomeModel.cs
@@ -8,5 +8,10 @@ namespace NonProfitCRM.Models
     public class HomeModel
     {
         public IEnumerable<ViewTaskList> TaskList { get; set; }
+        public IEnumerable<ViewEventList> EventList { get; set; }
+        public IEnumerable<ViewCompanyList> CompanyList { get; set; }
+        public IEnumerable<ViewNonProfitOrgList> NonProfitOrgList { get; set; }
+        public string Search { get; set; }
+
     }
 }
