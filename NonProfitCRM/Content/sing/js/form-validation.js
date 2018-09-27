7a35376920594d8a2c997fd350143506f612e9f5
valda-z <valdemar.zavadsky@microsoft.com>
2018-09-27 13:38:09 -0400

    ui experience finetuned

--- NonProfitCRM/Content/sing/js/form-validation.js
+++ NonProfitCRM/Content/sing/js/form-validation.js
@@ -2,7 +2,20 @@ $(function(){
     function pageLoad(){
         $('.widget').widgster();
         //init parsley for pjax requests
-        $( '#validation-form' ).parsley();
+        $('#validation-form').parsley({
+            excluded:
+                // Defaults
+                'input[type=radio],' +
+                'input[type=button],' +
+                'input[type=submit],' +
+                'input[type=reset],' +
+                'input[type=hidden],' +
+                '[disabled],' +
+                ':hidden,' +
+                // -- Additional attributes to look --
+                '[data-parsley-disabled],' +  // Exclude specific input/select/radio/checkbox/etc
+                '[data-parsley-disabled] *'   // Exclude all nesting inputs/selects/radios/checkboxes/etc
+        });
     }
     pageLoad();
     SingApp.onPageLoad(pageLoad);
